46c2ebed7e149dc52b6ec20b5471b2126764f71d
venkannababua <venkannababu.a@hqledutech.com>
2024-12-12 11:45:21 +0530

    bugfixed1

---
+++ AddDemo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AddDemo {
+public static void main(String[] args) {
+    int a,b,c;
+    Scanner scan=new Scanner(System.in);
+    System.out.println("enter a");
+    a=scan.nextInt();
+    System.out.println("enter b");
+    b=scan.nextInt();
+    c=a+b;
+    System.out.println(c);
+    scan.close();
+}
+    
+}
