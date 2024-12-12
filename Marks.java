f1d49396e34e7fb789de45d7e9eddfa5ceb7bd00
Naga Vamsi Krishna <nagavamsikrishna129@gmail.com>
2024-12-12 12:13:12 +0530

    Welcome

---
+++ Marks.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Marks {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter marks: ");
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();  
+            sum += arr[i]; 
+        }
+        System.out.println(sum);
+        double avg = sum/(float)arr.length;
+        System.out.println("The average marks are: " + avg);
+        
+
+    }
+    
+}
