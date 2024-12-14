1ab6a40995da965cf1295c0d1d129bb4b61c881d
venky1258 <140711690+venky1258@users.noreply.github.com>
2024-12-14 12:17:11 +0530

    Update td.java

--- td.java
+++ td.java
@@ -21,9 +21,12 @@ class td
             }
         }      
 
+
+        
         for(i=0;i<a.length;i++)
           for(j=0;j<a[i].length;j++)
             c[i][j]=a[i][j]+b[i][j];
+        
 
         
         System.out.println("c array values");
