d0a4c695fa42e5d201f291bcf096289031694ff7
venkannababua <venkannababu.a@hqledutech.com>
2024-12-18 17:48:45 +0530

    t26

--- ttt.java
+++ ttt.java
@@ -1,7 +1,7 @@
 public class ttt {
     public static void main(String[] args) {
         System.out.println("www");
-        System.out.println();
+        System.out.println("weewe");
     
     
     }
