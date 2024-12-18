351d0c094097b815aac9a33f9180b53ee3b381c6
venkannababua <venkannababu.a@hqledutech.com>
2024-12-18 17:39:00 +0530

    t24

--- yyyy.java
+++ yyyy.java
@@ -1,8 +1,6 @@
-public class ttt {
+public class yyyy {
     public static void main(String[] args) {
-        System.out.println("www");
         System.out.println();
-    
-    
+        System.out.println();
     }
 }
